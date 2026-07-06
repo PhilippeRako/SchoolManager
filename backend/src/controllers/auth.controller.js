778507810c9634de6e756ff50044c2522482322d
PhilippeRako <philrakotondrabe@gmail.com>
2026-07-06 20:44:07 +0300

    Add - functionnal login page

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,37 @@
-exports.login = (req, res) => {
-    res.json({ message: 'Login OK' });
+const db = require('../config/db');
+const jwt = require('jsonwebtoken');
+
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+
+    const [rows] = await db.query(
+        'SELECT * FROM users WHERE email = ?',
+        [email]
+    );
+
+    if (rows.length === 0) {
+        return res.status(401).json({ error: 'Email incorrect' });
+    }
+
+    const user = rows[0];
+
+
+    if (user.password !== password) {
+        return res.status(401).json({ error: 'Mot de passe incorrect' });
+    }
+
+
+    const token = jwt.sign(
+        { id: user.id, role: user.role },
+        'SECRET_KEY',
+        { expiresIn: '1d' }
+    );
+
+    res.json({
+        message: 'Connexion réussie',
+        token,
+        role: user.role,
+        nom: user.nom,
+        prenom: user.prenom
+    });
 };
--- frontend/src/pages/Login.vue
+++ frontend/src/pages/Login.vue
@@ -1,6 +1,61 @@
 <template>
-  <div class="container mt-5">
-    <h2>Connexion</h2>
-    <p>Page Login prête.</p>
+  <div class="container mt-5" style="max-width: 400px;">
+    <h3 class="mb-3">Connexion</h3>
+
+    <form @submit.prevent="login">
+      <div class="mb-3">
+        <label>Email</label>
+        <input v-model="email" type="email" class="form-control" required>
+      </div>
+
+      <div class="mb-3">
+        <label>Mot de passe</label>
+        <input v-model="password" type="password" class="form-control" required>
+      </div>
+
+      <button class="btn btn-primary w-100">Se connecter</button>
+    </form>
+
+    <p class="text-danger mt-3">{{ error }}</p>
   </div>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      error: ''
+    };
+  },
+  methods: {
+    async login() {
+      try {
+        const res = await axios.post('http://localhost:3000/api/auth/login', {
+          email: this.email,
+          password: this.password
+        });
+
+
+        localStorage.setItem('token', res.data.token);
+        localStorage.setItem('role', res.data.role);
+        localStorage.setItem('nom', res.data.nom);
+        localStorage.setItem('prenom', res.data.prenom);
+
+
+        if (res.data.role === 'super_admin') {
+          this.$router.push('/dashboard-super-admin');
+        } else {
+          this.$router.push('/dashboard-admin');
+        }
+
+      } catch (err) {
+        this.error = err.response?.data?.error || 'Erreur de connexion';
+      }
+    }
+  }
+};
+</script>
