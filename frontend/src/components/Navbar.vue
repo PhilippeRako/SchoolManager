f67f598aac868b50b4dd916a87c3849fb1850bcf
PhilippeRako <philrakotondrabe@gmail.com>
2026-07-07 12:33:19 +0300

    Add - theme + navbar

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,5 +1,55 @@
 <template>
-  <nav class="navbar navbar-dark bg-dark px-3">
+  <nav class="navbar navbar-expand-lg navbar-modern px-3">
     <a class="navbar-brand" href="#">SchoolManager</a>
+
+    <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <router-link class="nav-link" to="/gestion-eleves">
+            Gestion élèves
+          </router-link>
+        </li>
+
+        <li class="nav-item">
+          <router-link class="nav-link" to="/gestion-profs">
+            Gestion profs
+          </router-link>
+        </li>
+      </ul>
+
+      <span class="navbar-text me-3">
+        {{ prenom }} {{ nom }}
+      </span>
+
+      <button class="btn btn-sm btn-logout" @click="logout">
+        Déconnexion
+      </button>
+    </div>
   </nav>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      nom: localStorage.getItem('nom'),
+      prenom: localStorage.getItem('prenom')
+    };
+  },
+  methods: {
+    logout() {
+      localStorage.clear();
+      this.$router.push('/login');
+    }
+  }
+};
+</script>
